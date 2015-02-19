2475b64a910b66c1d7643e8a309a4148c8f660ee
caseydedore <caseydedore@gmail.com>
2015-02-19 11:49:51 -0800

    Post model DateTime format
    Small changes to Post model. DateTime is now Date for  database storage.
    The display is now formatted to remove needless info such as hour,
    minute, second.

--- DigitalArtistDatabase/Models/Post.cs
+++ DigitalArtistDatabase/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,8 @@ namespace DigitalArtistDatabase.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMMM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DatePosted { get; set; }
         public uint ViewCount { get; set; }
 
